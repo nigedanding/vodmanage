0b89f5d1e8316b309ac4a7d3a01f44e2c85916b7
nigedanding <1961790210@qq.com>
2020-06-16 11:18:34 +0800

    后台

---
+++ __init__.py
@@ -0,0 +1,25 @@
+#coding:utf8
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+
+import pymysql,os
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:mysql@127.0.0.1/movie"
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = True
+app.config["SECRET_KEY"]="ZHANGSan"
+app.config["UP_DIR"]= os.path.join(os.path.abspath(os.path.dirname(__file__)),'static/upload/')
+app.debug = True
+db = SQLAlchemy(app)
+
+
+from  app.home import  home as home_blueprint
+from  app.admin import admin as  admin_blueprint
+
+app.register_blueprint(home_blueprint)
+app.register_blueprint(admin_blueprint,url_prefix="/admin")
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return  render_template("home/404.html"),404
+
